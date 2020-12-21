3cc013df9066d28cd95a0fe5f3031e689a4781af
simoustro30 <simoustro30@users.noreply.github.com>
2020-12-21 17:57:34 -0500

    add chat avatar

--- src/Chats.js
+++ src/Chats.js
@@ -1,9 +1,32 @@
 import React from 'react'
-
+import Chat from './Chat'
 function Chats() {
     return (
-        <div>
-            
+        <div className='chats'>
+            <Chat
+                name= "Sarah"
+                message="Hey! Wyd?"
+                timestamp= '35 mins ago'
+                profilePic= ''
+            />
+            <Chat
+                name= "Alexa"
+                message="Wow"
+                timestamp= '21 mins ago'
+                profilePic= ''            
+            />
+            <Chat
+                name= "Natasha"
+                message="This is my chance"
+                timestamp= '5 days ago'
+                profilePic= ''            
+            />
+            <Chat
+                name= "Elon"
+                message="We are hiring now o.O"
+                timestamp= '12 mins ago'
+                profilePic= ''            
+            />
         </div>
     )
 }
